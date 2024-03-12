c8c63c196812a13588583cad84464c8d0719075b
Ali Ahmed Samoo <aliahmed.samoo.1@gmail.com>
2024-03-13 04:17:38 +0500

    ..

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 // require('./db/connection');
 // // const axios = require('axios');
@@ -9,9 +9,9 @@ const app = express();
 
 
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors())
 
 app.get('/', (req, res) => {
     const date = Date.now();
